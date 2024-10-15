ced200cf1e0078313c8e2216c40c52b2f85496ff
Chris Burgin <chris.burgin@agilebits.com>
2024-10-15 13:56:49 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Papa from "papaparse";
 
 function App() {
+  const [cards, setCards] = useState(undefined);
+
+  const uploadFile = async (event) => {
+    const text = await event.target.files[0].text();
+    const parsed = Papa.parse(text).data;
+    delete parsed[0];
+
+    const res = parsed.map((item) => ({
+      name: item[2],
+      team: item[4],
+    }));
+
+    setCards(res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={uploadFile} className="upload" />
+
+      <div className="card-wrapper">
+        {cards &&
+          cards.map((card) => (
+            <div className="card">
+              <div className="name"> {card.name}</div>
+              <div className="divider" />
+              <div className="team"> {card.team}</div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
