3af4e810b3abb5ec35e54fc10e67c38af92c18df
Chris Burgin <chris.burgin@agilebits.com>
2024-10-15 14:27:56 -0400

    improve

--- src/App.js
+++ src/App.js
@@ -3,32 +3,67 @@ import { useState } from "react";
 import Papa from "papaparse";
 
 function App() {
-  const [cards, setCards] = useState(undefined);
+  const [fields, setFields] = useState({});
+  const [header, setHeader] = useState(undefined);
+  const [data, setData] = useState(undefined);
 
   const uploadFile = async (event) => {
     const text = await event.target.files[0].text();
     const parsed = Papa.parse(text).data;
+
+    setHeader(parsed[0]);
     delete parsed[0];
 
-    const res = parsed.map((item) => ({
-      name: item[2],
-      team: item[4],
-    }));
-
-    setCards(res);
+    setData(parsed);
   };
 
   return (
     <div className="App">
-      <input type="file" onChange={uploadFile} className="upload" />
+      <div className="controls">
+        <input type="file" onChange={uploadFile} className="upload" />
+        <div className="selects">
+          <label>
+            Primary
+            <select
+              name="primary"
+              onChange={(event) =>
+                setFields({
+                  ...fields,
+                  primary: event.target.selectedIndex - 1,
+                })
+              }
+            >
+              <option></option>
+              {header && header.map((entry) => <option> {entry} </option>)}
+            </select>
+          </label>
+
+          <label>
+            Secondary
+            <select
+              name="secondary"
+              onChange={(event) =>
+                setFields({
+                  ...fields,
+                  secondary: event.target.selectedIndex - 1,
+                })
+              }
+            >
+              <option></option>
+              {header && header.map((entry) => <option> {entry} </option>)}
+            </select>
+          </label>
+        </div>
+      </div>
 
       <div className="card-wrapper">
-        {cards &&
-          cards.map((card) => (
+        {data &&
+          fields.primary &&
+          data.map((entry) => (
             <div className="card">
-              <div className="name"> {card.name}</div>
+              <div className="name">{entry[fields.primary]}</div>
               <div className="divider" />
-              <div className="team"> {card.team}</div>
+              <div className="team">{entry[fields.secondary]}</div>
             </div>
           ))}
       </div>
